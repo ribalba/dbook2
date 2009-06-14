00155176ab7d3dab3f30b3cc79021ff1f9b46a09
dan.wentworth <dan.wentworth@ece50b6e-583f-11de-a2a3-1fafad8a8996>
2009-06-14 01:40:34 +0000

    Created PyDict book_dict, converted data from structure book_details to dictionary

--- dbookpy/dbookpy.c
+++ dbookpy/dbookpy.c
@@ -65,6 +65,8 @@ static PyObject * get_isbn_details(PyObject * self, PyObject *args){
     dbook_book book_details;
     int rtn;
     
+    PyObject *book_dict = PyDict_New();
+    
     if(!PyArg_ParseTuple(args, "s", isbn)){
         return NULL;
     }
@@ -73,6 +75,18 @@ static PyObject * get_isbn_details(PyObject * self, PyObject *args){
     rtn = dbook_get_isbn_details(book_isbn, &book_details);
     
     /* TODO: Figure out how to translate book_details into a python obj */
+    PyDict_SetItem(book_dict, PyString_FromString("title"), PyString_FromString(book_details.title));
+    PyDict_SetItem(book_dict, PyString_FromString("author"), PyString_FromString(book_details.author));
+    PyDict_SetItem(book_dict, PyString_FromString("date"), PyString_FromString(book_details.date));
+    PyDict_SetItem(book_dict, PyString_FromString("publisher"), PyString_FromString(book_details.publisher));
+    PyDict_SetItem(book_dict, PyString_FromString("edition"), Py_BuildValue("d", book_details.edition));
+    PyDict_SetItem(book_dict, PyString_FromString("pagecount"), Py_BuildValue("d", book_details.pagecount));
+    PyDict_SetItem(book_dict, PyString_FromString("image_path"), PyString_FromString(book_details.image_path));
+    PyDict_SetItem(book_dict, PyString_FromString("category"), PyString_FromString(book_details.category));
+    PyDict_SetItem(book_dict, PyString_FromString("url"), PyString_FromString(book_details.url));
+    PyDict_SetItem(book_dict, PyString_FromString("booktype"), PyString_FromString(book_details.booktype));
+    
+    return book_dict;
 }
 
 static PyMethodDef dbook_methods[] = {
@@ -80,6 +94,7 @@ static PyMethodDef dbook_methods[] = {
     {"isbn_10_to_13", isbn_10_to_13, METH_VARARGS, "Convert ISBN10 to ISBN13"},
     {"isbn_13_to_13", isbn_13_to_10, METH_VARARGS, "Convert ISBN13 to ISBN10"},
     {"sanitize", sanitize, METH_VARARGS, "Sanitize ISBN"},
+    {"get_isbn_details", get_isbn_details, METH_VARARGS, "Get Details on a book in a dict"},
     {NULL, NULL, 0, NULL},
 };
 
