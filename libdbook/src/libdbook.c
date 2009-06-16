7f8f5f1024f7373795bb85f9fa56b6ad4df6ec63
vext01 <vext01@ece50b6e-583f-11de-a2a3-1fafad8a8996>
2009-06-16 02:23:19 +0000

    big clean up! we need a coding standard

--- libdbook/src/libdbook.c
+++ libdbook/src/libdbook.c
@@ -7,27 +7,27 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "libdbook.h"
-#include "liblocaldbook.c"
 
 char serverToTalkTo[50];
-int onlineMode = DBOOK_FALSE;
+int onlineMode = DBOOK_FALSE; /* XXX is this still needed? */
 
-int dbook_check_isbn(DBOOK_ISBN *isbnToCheck){
+int dbook_check_isbn(DBOOK_ISBN *isbnToCheck) {
     if (onlineMode == DBOOK_FALSE){
         return dbook_check_isbn_loc(isbnToCheck);
-    }else{
-        //dbook_sanitize_dis(from, to);
+    } else {
+        /* dbook_sanitize_dis(from, to); */
     }
 
     return DBOOK_FALSE;
 }
 
-int dbook_isbn_10_to_13(DBOOK_ISBN *from, DBOOK_ISBN *to){
-    if (onlineMode == DBOOK_FALSE){
-       return dbook_isbn_10_to_13_loc(from, to);
-    }else{
-        //dbook_sanitize_dis(from, to);
+int dbook_isbn_10_to_13(DBOOK_ISBN *from, DBOOK_ISBN *to) {
+    if (onlineMode == DBOOK_FALSE) {
+        return dbook_isbn_10_to_13_loc(from, to);
+    } else {
+        /* dbook_sanitize_dis(from, to); */
     }
 
     return DBOOK_FALSE;
@@ -35,10 +35,10 @@ int dbook_isbn_10_to_13(DBOOK_ISBN *from, DBOOK_ISBN *to){
 }
 
 int dbook_isbn_13_to_10(DBOOK_ISBN *from, DBOOK_ISBN *to){
-    if (onlineMode == DBOOK_FALSE){
-       return dbook_isbn_13_to_10_loc(from, to);
-    }else{
-        //dbook_sanitize_dis(from, to);
+    if (onlineMode == DBOOK_FALSE) {
+        return dbook_isbn_13_to_10_loc(from, to);
+    } else {
+        /* dbook_sanitize_dis(from, to); */
     }
 
     return DBOOK_FALSE;
@@ -48,7 +48,7 @@ int dbook_sanitize(char *from, DBOOK_ISBN *to){
     if (onlineMode == DBOOK_FALSE){
         dbook_sanitize_loc(from, to);
     }else{
-        //dbook_sanitize_dis(from, to);
+        /* dbook_sanitize_dis(from, to); */
     }
 
     return DBOOK_TRUE;
@@ -79,84 +79,84 @@ int dbook_is_isbn_13(DBOOK_ISBN *isbnToCheck){
     if (onlineMode == DBOOK_FALSE){
         return dbook_is_isbn_13_loc(isbnToCheck);
     }else{
-        //dbook_sanitize_dis(from, to);
+        /* dbook_sanitize_dis(from, to); */
     }
     return DBOOK_FALSE;
 }
 
-int dbook_is_isbn_10(DBOOK_ISBN *isbnToCheck){
-    if (onlineMode == DBOOK_FALSE){
+int dbook_is_isbn_10(DBOOK_ISBN *isbnToCheck) {
+    if (onlineMode == DBOOK_FALSE) {
         return dbook_is_isbn_10_loc(isbnToCheck);
-    }else{
-        //dbook_sanitize_dis(from, to);
-    }
-    return DBOOK_FALSE;
-
-}
-
-char dbook_genChkSum10(DBOOK_ISBN *isbnToTest){
-    if (onlineMode == DBOOK_FALSE){
-        return dbook_genChkSum10_loc(isbnToTest);
-    }else{
-        //dbook_sanitize_dis(from, to);
+    } else {
+        /* dbook_sanitize_dis(from, to); */
     }
     return DBOOK_FALSE;
 }
 
-char dbook_genChkSum13(DBOOK_ISBN *isbnToTest){
-    if (onlineMode == DBOOK_FALSE){
-        return dbook_genChkSum13_loc(isbnToTest);
-    }else{
-        //dbook_sanitize_dis(from, to);
+char dbook_gen_chksum_10(DBOOK_ISBN *isbnToTest) {
+
+    if (onlineMode == DBOOK_FALSE) {
+        return dbook_gen_chksum_10_loc(isbnToTest);
+    } else {
+        /* dbook_sanitize_dis(from, to); */
+    }
+    return DBOOK_FALSE;
+}
+
+char dbook_gen_chksum_13(DBOOK_ISBN *isbnToTest) {
+    if (onlineMode == DBOOK_FALSE) {
+        return dbook_gen_chksum_13_loc(isbnToTest);
+    } else {
+        /* dbook_sanitize_dis(from, to); */
     }
     return DBOOK_FALSE;
 }
 
 char *dbook_filter_book_plain(dbook_book *bk) {
-	/*
-	printf("ISBN:		%s\n", bk.isbn);
-	printf("Title:		%s\n", bk.title);
-	printf("Author:		%s\n", bk.author);
-	printf("Date:		%s\n", bk.date);
-	printf("Publisher:	%s\n", bk.publisher);
-	printf("Edition:	%d\n", bk.edition);
-	printf("Pages:		%d\n", bk.pagecount);
-	printf("Image Path:	%s\n", bk.image_path);
-	printf("Category:	%s\n", bk.category);
-	printf("URL:		%s\n", bk.url);
-	printf("Type:		%s\n", bk.booktype);
-	*/
-	return 0; /* XXX fixme */
+    /*
+       printf("ISBN:		%s\n", bk.isbn);
+       printf("Title:		%s\n", bk.title);
+       printf("Author:		%s\n", bk.author);
+       printf("Date:		%s\n", bk.date);
+       printf("Publisher:	%s\n", bk.publisher);
+       printf("Edition:	%d\n", bk.edition);
+       printf("Pages:		%d\n", bk.pagecount);
+       printf("Image Path:	%s\n", bk.image_path);
+       printf("Category:	%s\n", bk.category);
+       printf("URL:		%s\n", bk.url);
+       printf("Type:		%s\n", bk.booktype);
+       */
+    return 0; /* XXX fixme */
 }
 
 char *dbook_filter_book_bibtex(dbook_book *bk) {
-	char *lines[9]; /* line 10 is always just a close brace */
-	char *ret = "";
-	int sz, ret_sz = 0;
-	int i;
-	const int btex_lines = 2;
+    char *lines[9]; /* line 10 is always just a close brace */
+    char *ret = "";
+    int sz, ret_sz = 0;
+    int i;
+    const int btex_lines = 2;
 
-	/* key line */
-	sz = 9 + strlen(bk->isbn); /* ie, 7 + \n + \0 + isbn */
-	lines[0] = (char *) malloc(sz);
-	snprintf(lines[0], sz, "@BOOK{%s,\n", bk->isbn);
+    /* key line */
+    sz = 9 + strlen(bk->isbn); /* ie, 7 + \n + \0 + isbn */
+    lines[0] = (char *) malloc(sz);
+    snprintf(lines[0], sz, "@BOOK{%s,\n", bk->isbn);
 
-	/* author line */
-	sz = 15 + strlen(bk->author);
-	lines[1] = (char *) malloc(sz);
-	snprintf(lines[1], sz, "\tauthor = \"%s\",\n", bk->author);
+    /* author line */
+    sz = 15 + strlen(bk->author);
+    lines[1] = (char *) malloc(sz);
+    snprintf(lines[1], sz, "\tauthor = \"%s\",\n", bk->author);
 
-	/* XXX and the rest */
+    /* XXX and the rest */
 
-	for (i = 0; i < btex_lines; i ++) {
-		ret_sz = ret_sz + strlen(lines[i]);
-	}
+    for (i = 0; i < btex_lines; i ++) {
+        ret_sz = ret_sz + strlen(lines[i]);
+    }
 
-	ret = (char *) malloc(sz) + 1;
-	for (i = 0; i < btex_lines; i ++) {
-		strncat(ret, lines[i], strlen(lines[i]));
-		free(lines[i]);
-	}
+    ret = (char *) malloc(sz) + 1;
+    for (i = 0; i < btex_lines; i ++) {
+        strncat(ret, lines[i], strlen(lines[i]));
+        free(lines[i]);
+    }
 
-	return ret;
+    return ret;
 }
--- libdbook/src/libdistdbook.c
+++ libdbook/src/libdistdbook.c
@@ -1,4 +1,4 @@
-'/*
+/*
  * $Id$
  * -----------------------
  * The distributed dbook thing
@@ -13,85 +13,86 @@
 #include <strings.h>
 #include "libdbook.h"
 
-int dbook_connect_send_wait_receive(void* input)
-{
-  int sockfd;
-  int portno;
-  int n;
-  struct sockaddr_in serv_addr;
-  struct hostent *server;
-  char buffer[512];
+/*
+ * XXX
+ * This is not used just yet, is carston still hacking this?
+ * This is C++ !!!!!!!!!!!!!!!!!!!!!!!!!!
+ * The library should all be pure C99!
+ */
 
-  if (argc < 2)
-  {
-    cout << "Error: No host specified" << endl;
-    return 1;
-  }
+int dbook_connect_send_wait_receive(void* input) {
+    int sockfd;
+    int portno;
+    int n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[512];
 
-  if (argc < 3)
-  {
-    cout << "Error: No port specified" << endl;
-    return 1;
-  }
+    if (argc < 2) {
+        cout << "Error: No host specified" << endl;
+        return 1;
+    }
 
-  portno = atoi(argv[2]);
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (argc < 3) {
+        cout << "Error: No port specified" << endl;
+        return 1;
+    }
 
-  if(sockfd < 0)
-  {
-    cout << "Error: Could not open socket" << endl;
-    return 1;
-  }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-  server = gethostbyname(argv[1]);
+    if(sockfd < 0) {
+        cout << "Error: Could not open socket" << endl;
+        return 1;
+    }
 
-  if(server == NULL)
-  {
-    cout << "Error: Could not find host" << endl;
-    return 1;
-  }
+    server = gethostbyname(argv[1]);
 
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-  serv_addr.sin_port = htons(portno);
+    if(server == NULL) {
+        cout << "Error: Could not find host" << endl;
+        return 1;
+    }
 
-  if(connect(sockfd, (const sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-  {
-    cout << "Error: Could not connect" << endl;
-    return 1;
-  }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+            (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
 
-  printf("Please enter the message: ");
-  bzero(buffer, 512);
-  fgets(buffer, 511, stdin);
-  n = write(sockfd,buffer,strlen(buffer));
+    if (connect(sockfd, (const sockaddr*)&serv_addr,
+                sizeof(serv_addr)) < 0) {
+        cout << "Error: Could not connect" << endl;
+        return 1;
+    }
 
-  if (n < 0)
-  {
-    cout << "Error: Could not write to socket" << endl;
-    return 1;
-  }
+    printf("Please enter the message: ");
+    bzero(buffer, 512);
+    fgets(buffer, 511, stdin);
+    n = write(sockfd,buffer,strlen(buffer));
 
-  bzero(buffer, 512);
-  n = read(sockfd, buffer, 511);
+    if (n < 0) {
+        cout << "Error: Could not write to socket" << endl;
+        return 1;
+    }
 
-  if(n < 0)
-  {
-    cout << "Error: Could not read from socket" << endl;
-    return 1;
-  }
+    bzero(buffer, 512);
+    n = read(sockfd, buffer, 511);
 
-  cout << "Received: " << n << " bytes" << endl;
-  cout << "Message: " << buffer << endl;
+    if(n < 0) {
+        cout << "Error: Could not read from socket" << endl;
+        return 1;
+    }
 
-  return 0;
+    cout << "Received: " << n << " bytes" << endl;
+    cout << "Message: " << buffer << endl;
+
+    return 0;
 }
 
 /** 
   Returns the checksum for a ISBN 10 passed in as paramter
   */
-char dbook_genChkSum10_dis(DBOOK_ISBN *isbnToTest) {
+char dbook_gen_chksum_10_dis(DBOOK_ISBN *isbnToTest) {
     return 'X';
 }
 
@@ -99,21 +100,19 @@ char dbook_genChkSum10_dis(DBOOK_ISBN *isbnToTest) {
 /** 
   Returns the checksum for a ISBN 13 passed in as paramter
   */
-char dbook_genChkSum13_dis(DBOOK_ISBN *isbnToTest) {
+char dbook_gen_chkSum_13_dis(DBOOK_ISBN *isbnToTest) {
     return 'X';
 }
 
-
-
-int dbook_check_isbn_dis(DBOOK_ISBN *isbnToCheck){
+int dbook_check_isbn_dis(DBOOK_ISBN *isbnToCheck) {
     return DBOOK_FALSE;
 }
 
-int dbook_isbn_10_to_13_dis(DBOOK_ISBN *from, DBOOK_ISBN *to){
+int dbook_isbn_10_to_13_dis(DBOOK_ISBN *from, DBOOK_ISBN *to) {
     return DBOOK_FALSE;
 }
 
-int dbook_isbn_13_to_10_dis(DBOOK_ISBN *from, DBOOK_ISBN *to){
+int dbook_isbn_13_to_10_dis(DBOOK_ISBN *from, DBOOK_ISBN *to) {
     return DBOOK_FALSE;
 }
 
@@ -122,26 +121,25 @@ int dbook_isbn_13_to_10_dis(DBOOK_ISBN *from, DBOOK_ISBN *to){
  * first value is a char * to be cleaned
  * second is a dbook_isbn for the clean isbn to be put in
  */ 
-int dbook_sanitize_dis(char *from, DBOOK_ISBN *to){
+int dbook_sanitize_dis(char *from, DBOOK_ISBN *to) {
     return DBOOK_FALSE;
 }
 
-int dbook_get_isbn_details_dis(DBOOK_ISBN *whichBook, dbook_book *book){
+int dbook_get_isbn_details_dis(DBOOK_ISBN *whichBook, dbook_book *book) {
     return DBOOK_FALSE;
 }
-
 
 /**
  * Checks if the isbn is a isbn 13. Does not validate only takes the length
  */
-int dbook_is_isbn_13_dis(DBOOK_ISBN *isbnToCheck){
+int dbook_is_isbn_13_dis(DBOOK_ISBN *isbnToCheck) {
     return DBOOK_FALSE;
 }
 
 /**
  * Checks if the isbn is a isbn 10. Does not validate only takes the length
  */
-int dbook_is_isbn_10_dis(DBOOK_ISBN *isbnToCheck){
+int dbook_is_isbn_10_dis(DBOOK_ISBN *isbnToCheck) {
     return DBOOK_FALSE; 
 }
 
