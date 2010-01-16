16db62e4893af18bf6b9829fb1ea29edd763488b
vext01 <vext01@ece50b6e-583f-11de-a2a3-1fafad8a8996>
2010-01-16 21:54:13 +0000

    redo dbook_item to didi's new spec.

--- trunk/libdbook/src/dbook.h
+++ trunk/libdbook/src/dbook.h
@@ -12,7 +12,7 @@
 #define DBOOK_FALSE	1
 
 /* 10 or 13 long plus \0 */
-#define DBOOK_ISBN_LEN 14
+#define DBOOK_MAX_ISBN 14
 #define DBOOK_CHAR char
 
 /* Error Codes */
@@ -46,24 +46,33 @@ extern int  dbook_bkends_in_use;
 
 typedef DBOOK_CHAR dbook_isbn[];
 
-#define XXX
-typedef struct dbook_book_ {
-    DBOOK_CHAR isbn[DBOOK_ISBN_LEN];
-    char title[250];
-    char author[250];
-    char date[50];
-    char publisher[250];
-    int  edition;
-    int  pagecount;
-    char image_path[250];
-    char category[250];
-    char url[250];
-    char booktype[50];
-} dbook_book;
+#define DBOOK_MAX_FIELD 256
+typedef struct dbook_item_ {
+    int type;
+    DBOOK_CHAR  author[DBOOK_MAX_FIELD];
+    DBOOK_CHAR  created[DBOOK_MAX_FIELD];
+    int         dtype;
+    int         edition;
+    DBOOK_CHAR  editor[DBOOK_MAX_FIELD];
+    u_int64_t   id;
+    DBOOK_CHAR  institution[DBOOK_MAX_FIELD];
+    DBOOK_CHAR  isbn[DBOOK_MAX_ISBN];
+    DBOOK_CHAR  journal[DBOOK_MAX_FIELD];
+    DBOOK_CHAR  note[DBOOK_MAX_FIELD];
+    DBOOK_CHAR  osbn[DBOOK_MAX_FIELD];
+    int         pages;
+    DBOOK_CHAR  pubdate[9]; /* DD-MM-YYYY\0 */
+    DBOOK_CHAR  publisher[DBOOK_MAX_FIELD];
+    /* XXX servies */
+    DBOOK_CHAR  source[DBOOK_MAX_FIELD];
+    DBOOK_CHAR  title[DBOOK_MAX_FIELD];
+    DBOOK_CHAR  modified[DBOOK_MAX_FIELD];
+    int         volume;
+} dbook_item;
 
 typedef struct dbook_bkend_ {
     DBOOK_CHAR name[DBOOK_MAX_BKEND_NAME];
-    int (*get_isbn_details_func) (DBOOK_CHAR *, dbook_book *);
+    int (*get_isbn_details_func) (DBOOK_CHAR *, dbook_item *);
 } dbook_bkend;
 
 /* list of *registered* backends, like a nsswitch for dbook backends.
@@ -85,7 +94,7 @@ int dbook_check_isbn(DBOOK_CHAR *isbnToCheck);
 int dbook_isbn_10_to_13(DBOOK_CHAR *from, DBOOK_CHAR *to);
 int dbook_isbn_13_to_10(DBOOK_CHAR *from, DBOOK_CHAR *to);
 int dbook_sanitize(char *from, DBOOK_CHAR *to);
-int dbook_get_isbn_details(DBOOK_CHAR *whichBook, dbook_book *book);
+int dbook_get_isbn_details(DBOOK_CHAR *whichBook, dbook_item *book);
 int dbook_is_isbn_13(DBOOK_CHAR *isbnToCheck);
 int dbook_is_isbn_10(DBOOK_CHAR *isbnToCheck);
 char dbook_gen_chksum_10(DBOOK_CHAR *isbnToTest);
@@ -95,14 +104,14 @@ int dbook_check_initialised();
 int dbook_initialise();
 
 /* output_filters (filters.c) */
-char *dbook_filter_book_plain(dbook_book *book);
-char *dbook_filter_book_bibtex(dbook_book *book);
+char *dbook_filter_book_plain(dbook_item *book);
+char *dbook_filter_book_bibtex(dbook_item *book);
 
 /* debugging bits */
 void dbook_debug(char *);
 void dbook_perror();
 
 /* dbook.org backend functions */
-int dbook_org_get_isbn_details(DBOOK_CHAR *isbn, dbook_book *book);
+int dbook_org_get_isbn_details(DBOOK_CHAR *isbn, dbook_item *book);
 
 #endif
