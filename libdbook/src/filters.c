14a923f84b87d6cf955e3cdb9d7ffbb75ee3d0cd
vext01 <vext01@ece50b6e-583f-11de-a2a3-1fafad8a8996>
2010-01-19 05:23:56 +0000

    * fix bugs in xml code * tidy

--- libdbook/src/filters.c
+++ libdbook/src/filters.c
@@ -39,9 +39,12 @@ DBOOK_CHAR *dbook_filter_book_bibtex(dbook_item *item) {
     };
 
     /* allocate the bibtex db entry heder and copy in */
-    sz = strlen("@ARTICLE {OSBN-") + strlen(item->osbn) + strlen(",\n") + 1;
+    sz = strlen("-- Fetched from dbook.org\n@ARTICLE {") +
+        strlen(item->osbn) + strlen(",\n") + 1;
+
     ret = xmalloc(sz);
-    snprintf(ret, sz, "@ARTICLE {OSBN-%s,\n", item->osbn);
+    snprintf(ret, sz, "-- Fetched from dbook.org\n@ARTICLE {%s,\n",
+        item->osbn);
 
     for (i = 0; mapping[i] != (DBOOK_CHAR *) -1; i += 2) {
         /* if used, reallocate more space and copy in */
@@ -56,78 +59,14 @@ DBOOK_CHAR *dbook_filter_book_bibtex(dbook_item *item) {
         }
     }
 
+    /* kill final , */
+    ret[sz - 3] = '\n';
+    ret[sz - 2] = '\0';
+
+    /* close off record */
     sz = strlen(ret) + strlen("}\n") + 1;
     ret = realloc(ret, sz);
     snprintf(ret, sz, "%s}\n", ret);
 
-
     return ret;
-#if 0
-    char *lines[9]; /* line 10 is always just a close brace */
-    char *ret = "";
-    int sz, ret_sz = 0;
-    int i;
-    const int btex_lines = 10;
-
-    /* key line */
-    sz = 9 + strlen(bk->isbn); /* ie, 7 + \n + \0 + isbn */
-    lines[0] = (char *) malloc(sz);
-    snprintf(lines[0], sz, "@BOOK{%s,\n", bk->isbn);
-
-    /* title line */
-    sz = 14 + strlen(bk->title);
-    lines[1] = (char *) malloc(sz);
-    snprintf(lines[1], sz, "\ttitle = \"%s\",\n", bk->title);
-
-    /* author line */
-    sz = 15 + strlen(bk->author);
-    lines[2] = (char *) malloc(sz);
-    snprintf(lines[2], sz, "\tauthor = \"%s\",\n", bk->author);
-
-    /* year line */
-    sz = 13 + strlen(bk->date); /* XXX extract year only */
-    lines[3] = (char *) malloc(sz);
-    snprintf(lines[3], sz, "\tyear = \"%s\",\n", bk->date);
-
-    /* isbn line */
-    sz = 13 + strlen(bk->isbn);
-    lines[4] = (char *) malloc(sz);
-    snprintf(lines[4], sz, "\tisbn = \"%s\",\n", bk->isbn);
-
-    /* publisher line */
-    sz = 18 + strlen(bk->publisher);
-    lines[5] = (char *) malloc(sz);
-    snprintf(lines[5], sz, "\tpublisher = \"%s\",\n", bk->publisher);
-
-    /* edition line */
-    sz = 16 + sizeof(int);
-    lines[6] = (char *) malloc(sz);
-    snprintf(lines[6], sz, "\tedition = \"%d\",\n", bk->edition);
-
-    /* pages line */
-    sz = 14 + sizeof(int);
-    lines[7] = (char *) malloc(sz);
-    snprintf(lines[7], sz, "\tpages = \"%d\",\n", bk->pagecount);
-
-    /* url line */
-    sz = 12 + strlen(bk->url);
-    lines[8] = (char *) malloc(sz);
-    snprintf(lines[8], sz, "\turl = \"%s\"\n", bk->url);
-
-    /* close brace  - no \n on this*/
-    sz = 2;
-    lines[9] = (char *) malloc(sz);
-    snprintf(lines[9], sz, "}");
-
-    for (i = 0; i < btex_lines; i ++) {
-        ret_sz = ret_sz + strlen(lines[i]);
-    }
-
-    ret = (char *) malloc(ret_sz) + 1;
-    for (i = 0; i < btex_lines; i ++) {
-        strncat(ret, lines[i], strlen(lines[i]));
-        //free(lines[i]);
-    }
-#endif
-
 }
